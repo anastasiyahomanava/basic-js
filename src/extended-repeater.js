20956822715104aca5ba7ca557b21aa6935cab7e
Nastya <nastya_mi@icloud.com>
2020-03-22 21:44:45 +0300

    feat: repeater is done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,17 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let separator = options.separator;
+    let repeatTimes = options.repeatTimes;
+    let addition = options.addition;
+    let additionRepeatTimes = options.additionRepeatTimes;
+    let additionSeparator = options.additionSeparator;
+
+    separator = separator || '+';
+    additionSeparator = additionSeparator || '|';
+    addition = addition === null ? 'null' : addition;
+
+    let addString = new Array(additionRepeatTimes).fill(addition).join(additionSeparator);
+
+    let arr = new Array(repeatTimes).fill(str + addString).join(separator);
+
+    return arr;
 };
-  
