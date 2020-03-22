87330c5ae6ecb9f748a24ad812848f9add3085f4
Nastya <nastya_mi@icloud.com>
2020-03-22 16:03:50 +0300

    feat: what-season task is done

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,32 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date === undefined) {
+    return 'Unable to determine the time of year!';
+  } else if (!date.getMilliseconds()) {
+    throw Error;
+  }
+
+  const month = date.getMonth();
+
+  switch (month) {
+    case 2:
+    case 3:
+    case 4:
+      return 'spring';
+      break;
+    case 5:
+    case 6:
+    case 7:
+      return 'summer';
+      break;
+    case 8:
+    case 9:
+    case 10:
+      return 'autumn';
+      break;
+    case 11:
+    case 1:
+    case 0:
+      return 'winter';
+      break;
+  }
 };
