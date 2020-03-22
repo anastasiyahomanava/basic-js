f8c1050dbc8b7be867a9061f344fac20b90ba21b
Nastya <nastya_mi@icloud.com>
2020-03-22 20:25:57 +0300

    feat: transform-array is done

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,30 @@
 module.exports = function transform(arr) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (!Array.isArray(arr)) {
+        throw new Error();
+    } else if (arr.length === 0) {
+        return arr;
+    }
+
+    const transformedArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case '--discard-next':
+                i++;
+                break;
+            case '--discard-prev':
+                transformedArray.pop();
+                break;
+            case '--double-next':
+                if (i < arr.length - 1) transformedArray.push(arr[i + 1]);
+                break;
+            case '--double-prev':
+                if (i > 0) transformedArray.push(arr[i - 1]);
+                break;
+            default:
+                transformedArray.push(arr[i]);
+        }
+    }
+
+    return transformedArray;
 };
